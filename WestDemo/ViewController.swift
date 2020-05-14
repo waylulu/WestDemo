3e1a6b0451e23615a33eef062c62d7023e4355de
west <zhu3296697969@gmail.com>
2020-05-14 10:37:21 +0800

    create Demo

---
+++ WestDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WestDemo
+//
+//  Created by seven on 2020/5/14.
+//  Copyright © 2020 west. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
